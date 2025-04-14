e32881da8d97daeebe829f80b173e93211014299
Kalai-V <kalaiarasiv8@gmail.com>
2025-04-14 23:05:28 +0530

    Fixed typo in models

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ db = [
     {"name": "Srinithi", "age": 20, "gender": "Female","email": "sri@gmail.com"},
     {"name": "Archana", "age": 20, "gender": "Female","email": "archana@gmail.com"}
 ]
-
+logging.basicConfig(level=logging.INFO)
 def add_user(user: dict):
     logging.info(f"User added: {user}")
     db.append(user)
